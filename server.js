b17202717165bcac6da07346956c1f23149a045a
Rumkin <hello@rumk.in>
2016-07-14 17:24:01 +0000

    src: clean up wharf file; update server code

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
-var http = require('http');
+const http = require('http');
+const isJson = !! process.env.WHARF_OPT_APP_JSON;
+const message = 'Hello ' + process.env.WHARF_OPT_APP_GREETING;
 
 http.createServer((req, res) => {
-    res.end('Hello ' + process.env.WHARF_OPT_APP_GREETING);
+    if (isJson || (req.headers['content-type']||'').includes('application/json')) {
+        res.setHeader('content-type', 'application/json;charset=utf-8');
+        res.end(JSON.stringify({message}));
+    } else {
+        res.end(message);
+    }
 })
 .listen(8080, () => {
     process.env.WHARF_OPT_APP_VERBOSE && console.log('server started');
